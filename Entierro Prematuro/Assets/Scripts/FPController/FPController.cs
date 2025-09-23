6e891a86785e892075d39843d9a62db16bca0e19
Nodeannn <slicajuanmartin@gmail.com>
2025-09-23 11:31:20 -0300

    Started Proyect

---
+++ Entierro Prematuro/Assets/Scripts/FPController/FPController.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(CharacterController))]
+public class FPController : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    public float maxSpeed = 5f;
+    public float acceleration = 10f;
+
+    public Vector3 CurrentVelocity { get; private set; }
+    public float CurrentSpeed { get; private set; }
+
+    [Header("Look Settings")]
+    public Vector2 lookSensitivity = new Vector2(0.1f, 0.1f);
+
+    public float pitchLimit = 75f;
+
+    [SerializeField] float currentPitch = 0f;
+
+    public float CurrentPitch
+    {
+        get => currentPitch;
+
+        set
+        {
+            currentPitch = Mathf.Clamp(value, -pitchLimit, pitchLimit);
+        }
+    }
+
+    [Header("Input")]
+    public Vector2 moveInput;
+    public Vector2 lookInput;
+
+    [Header("Components")]
+    [SerializeField] Camera playerCamera;
+    [SerializeField] CharacterController characterController;
+
+    private void OnValidate()
+    {
+        if (playerCamera == null)
+        {
+            playerCamera = GetComponentInChildren<Camera>();
+        }
+
+        if (characterController == null)
+        {
+            characterController = GetComponent<CharacterController>();
+        }
+    }
+
+    private void Update()
+    {
+        moveUpdate();
+        lookUpdate();
+    }
+
+    void moveUpdate()
+    {
+        Vector3 motion = transform.forward * moveInput.y + transform.right * moveInput.x;
+        motion.y = 0f;
+        motion.Normalize();
+
+        if (motion.sqrMagnitude >= 0.01f)
+        {
+            CurrentVelocity = Vector3.MoveTowards(CurrentVelocity, motion * maxSpeed, acceleration * Time.deltaTime);
+        }
+        else
+        {
+            CurrentVelocity = Vector3.MoveTowards(CurrentVelocity, Vector3.zero, acceleration * Time.deltaTime);
+        }
+
+        float verticalVelocity = Physics.gravity.y * 20f * Time.deltaTime;
+
+        Vector3 fullVelocity = new Vector3(CurrentVelocity.x, verticalVelocity, CurrentVelocity.z);
+
+        characterController.Move(fullVelocity * maxSpeed * Time.deltaTime);
+
+        CurrentSpeed = CurrentVelocity.magnitude;
+    }
+
+    void lookUpdate()
+    {
+        Vector2 input = new Vector2(lookInput.x * lookSensitivity.x, lookInput.y * lookSensitivity.y);
+
+        // This is for Up and Down
+
+        CurrentPitch -= input.y;
+
+        playerCamera.transform.localRotation = Quaternion.Euler(CurrentPitch, 0f, 0f);
+
+        // This is for Left and Right
+
+        transform.Rotate(Vector3.up * input.x);
+    }
+}
+
