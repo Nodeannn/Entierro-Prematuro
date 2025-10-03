011d5db245215a8c3321cf20961043308dbdf50f
Nodeannn <slicajuanmartin@gmail.com>
2025-10-03 16:00:56 -0300

    PC Update

---
+++ Entierro Prematuro/Assets/StartSceneScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class StartSceneScript : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
