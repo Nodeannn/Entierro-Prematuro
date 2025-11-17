c7eee42a44b18cf01a9b01b1f6b3327d9b7e1fa1
Alessia Frey <lateacherdemate@gmail.com>
2025-11-17 16:25:53 -0300

    a

--- Entierro Prematuro/Assets/Scripts/Minijuego 1/Apagado Farol.cs
+++ Entierro Prematuro/Assets/Scripts/Minijuego 1/Apagado Farol.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class ApagadoFarol : MonoBehaviour
 {
     [SerializeField] private GameObject lightFarol;
+    [SerializeField] private GameObject light2Farol;
     [SerializeField] private float timeFarol = 3f;
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip sound;
@@ -19,6 +20,7 @@ public class ApagadoFarol : MonoBehaviour
         yield return new WaitForSeconds(timeFarol);
 
         lightFarol.SetActive(false);
+        light2Farol.SetActive(true);
         if (audioSource != null && sound != null)
         {
             audioSource.PlayOneShot(sound);
